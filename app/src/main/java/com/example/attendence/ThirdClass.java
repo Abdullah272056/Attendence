cdf99ef5e538e54fc146070fddef6bf87cfdd8ec
Abdullah272056 <abdullah272056@gmail.com>
2020-10-20 20:42:14 +0600

    add all student name delete function in Class3

--- app/src/main/java/com/example/attendence/ThirdClass.java
+++ app/src/main/java/com/example/attendence/ThirdClass.java
@@ -10,6 +10,9 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,6 +24,8 @@ import com.example.attendence.common.Notes;
 import com.example.attendence.common.Notes2;
 import com.example.attendence.mainpage.NameDataBaseHelperName;
 import com.example.attendence.mainpage.NoteClass;
+import com.example.attendence.ten_class.CustomAdapter10;
+import com.example.attendence.ten_class.DataBaseHelper10;
 import com.example.attendence.theme.ThemeDataBaseHelper;
 import com.example.attendence.theme.ThemeNote;
 import com.example.attendence.third_class.CustomAdapter3;
@@ -155,6 +160,36 @@ public class ThirdClass extends AppCompatActivity {
 
 
     }
+
+
+    //create option menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.studentNameDeleteItemId:
+                dataBaseHelper.deleteAllData();
+                dataBaseHelper=new DataBaseHelper3(context);
+                dataBaseHelper.getWritableDatabase();
+                studentInformationDataList= new ArrayList<>();
+                studentInformationDataList = dataBaseHelper.getAllNotes();
+                customAdapter = new CustomAdapter3(context,studentInformationDataList);
+                recyclerView.setAdapter(customAdapter);
+                return true;
+            case R.id.dateDeleteItemId:
+
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 
     private void loadStudentInformationData(){
         studentInformationDataList= new ArrayList<>();
