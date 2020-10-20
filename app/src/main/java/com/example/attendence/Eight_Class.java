e9ee4c2c37b9896d01d179e7246fd0a875332f9e
Abdullah272056 <abdullah272056@gmail.com>
2020-10-20 20:26:36 +0600

    add all student name delete function in Class8

--- app/src/main/java/com/example/attendence/Eight_Class.java
+++ app/src/main/java/com/example/attendence/Eight_Class.java
@@ -10,6 +10,9 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -25,6 +28,8 @@ import com.example.attendence.eight_class_date.DateCustomAdapter8;
 import com.example.attendence.eight_class_date.DateDataBaseHelper8;
 import com.example.attendence.mainpage.NameDataBaseHelperName;
 import com.example.attendence.mainpage.NoteClass;
+import com.example.attendence.ten_class.CustomAdapter10;
+import com.example.attendence.ten_class.DataBaseHelper10;
 import com.example.attendence.theme.ThemeDataBaseHelper;
 import com.example.attendence.theme.ThemeNote;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
@@ -153,6 +158,36 @@ public class Eight_Class extends AppCompatActivity {
 
 
     }
+
+    //create option menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.studentNameDeleteItemId:
+                dataBaseHelper.deleteAllData();
+                dataBaseHelper=new DataBaseHelper8(context);
+                dataBaseHelper.getWritableDatabase();
+                studentInformationDataList= new ArrayList<>();
+                studentInformationDataList = dataBaseHelper.getAllNotes();
+                customAdapter = new CustomAdapter8(context,studentInformationDataList);
+                recyclerView.setAdapter(customAdapter);
+                return true;
+            case R.id.dateDeleteItemId:
+
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
 
     private void loadStudentInformationData(){
         studentInformationDataList= new ArrayList<>();
